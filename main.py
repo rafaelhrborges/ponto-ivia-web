05bb639a0286cbff42456a805b2adabc69f1f06b
rafaelribeiro <rafaelektron@gmail.com>
2016-03-11 18:15:24 -0300

    capturando cookies

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask import render_template
 from flask.json import dumps
 
+from bs4 import BeautifulSoup
+
 import requests
 
 app = Flask(__name__)
@@ -12,15 +14,39 @@ def main():
 
 @app.route('/ponto')
 def login():
-    r = requests.post('http://intranet.ivia.com.br', data = {'txtUser': 'rafael.borges', 'txtPass': 'BObo220310', 'imageField': 'Login', 'imageField.x': '14', 'imageField.y': '32'})
-    print(dir(r.cookies.__dict__.get("_cookies")))
-    #r2 = requests.get('http://intranet.ivia.com.br/transfer.asp?targ=ponto')
+    s = requests.Session()
+    r = s.post('http://intranet.ivia.com.br', data = {'txtUser': 'rafael.borges', 'txtPass': 'BObo220310', 'imageField': 'Login', 'imageField.x': '14', 'imageField.y': '32'})
+    # print(dir(r.cookies.__dict__.get("_cookies")))
+    print(s.cookies.get_dict())
+    r2 = requests.get('http://intranet.ivia.com.br/transfer.asp?targ=ponto', cookies=s.cookies.get_dict())
     #r2 = requests.get('http://intranet.ivia.com.br/intranet.asp')
-    #import cgi
-    #return cgi.escape(r2.text)
-    #return dumps(r.cookies.__dict__)
+    # import cgi
+    # return cgi.escape(r2.text)
+    # return r2.text
+
+    soup = BeautifulSoup(r2.text)
+
+    fields = {
+        '__EVENTTARGET': '',
+        '__EVENTARGUMENT': '',
+        '__VIEWSTATE':'',
+        '__EVENTVALIDATION': '',
+        'CodeNumberTextBox': '',
+        'btnPonto': '',
+        'hdfLastTime': '',
+        'hdfTotalTime': '',
+        'hdfActivitiesCount': 0,
+        'txtHour': '',
+        'txtDescription': '',
+        'hdfDataEscolhida': '',
+    }
+
+    input = soup.find('input')
+    print(input)
+    import pdb; pdb.set_trace()
+    return dumps(s.cookies.get_dict())
     #return dumps(r.cookies.__dict__.get("_cookies").__dict__)
-    return 'ok'
+    # return 'ok'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
